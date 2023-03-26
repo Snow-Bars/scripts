0f5dc4b07e89ff9b16de4b55c8a83dff8c041fa7
Snow-Bars <121280889+Snow-Bars@users.noreply.github.com>
2023-03-26 16:46:15 +0300

    Update PVE_add_all_VMs_to_ha_resources.py

--- PVE_add_all_VMs_to_ha_resources.py
+++ PVE_add_all_VMs_to_ha_resources.py
@@ -15,9 +15,9 @@ for node in proxmox.nodes.get(): #get list of all nodes in the cluster over PVE 
     if node['status'] == 'online': #check node is online
         for vm in proxmox.nodes(node['node']).qemu.get(): #get all VMs from every node
             if vm['status'] == 'running' or vm['status'] == 'stopped': #check VM status is started or stopped
-                pci_dev = proxmox.nodes(node['node']).qemu(vm['vmid']).config.get() #get property of vm included pci pethgrouth divices
+                pci_dev = proxmox.nodes(node['node']).qemu(vm['vmid']).config.get() #get property of vm included pci passthrough divices
                 ha_state = proxmox.nodes(node['node']).qemu(vm['vmid']).status.current.get() #get vm ha-managed status property
-                if ('hostpci0' not in pci_dev) and (ha_state['ha']['managed'] != "0"): #check is vm have pce hardware in config and ha-managed status
+                if ('hostpci0' not in pci_dev) and (ha_state['ha']['managed'] != "0"): #check is vm have pci passthrough hardware in config and ha-managed status
                     subprocess.run(["ha-manager", "add", "vm:" + vm['vmid'], "--group", ha_group, "--state", vm['status']]) #set VM HA-managed with curent state
                 elif 'hostpci0' in pci_dev:
                     print('VM ', vm['vmid'], "got at least 1 pce passthrough device", pci_dev['hostpci0'], "and can't be ha-managed") #print info if VMhave pci hardware
